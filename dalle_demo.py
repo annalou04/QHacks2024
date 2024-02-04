0a8743e6bdedc0e984c20bbc99fa2f089ee40df8
Anna Lou <116932431+annalou04@users.noreply.github.com>
2024-02-03 23:39:19 -0500

    Update dalle_demo.py

--- dalle_demo.py
+++ dalle_demo.py
@@ -1,34 +1,66 @@
+"""
+This is a demo of using DALL-E-3 to create images from an audio file for QHACKS 2024.
+Author: Anna Lou
+Date: February 3, 2024
+"""
+# import stuff
 import openai
 from openai import OpenAI
 client = OpenAI()
 
+def transcribe_audio(audio_file_path):
+	"""Take in an audio file and transcribes it (Whisper model)"""  
+	audio_file= open(audio_file_path, "rb")
+	transcript = client.audio.transcriptions.create(
+		model="whisper-1", 
+		file=audio_file
+	)
+	print(transcript)
+	return(transcript)
+
 
 def image_prompt_from_text(transcription):
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": "Describe this scene using the following instructions: Use contemplative and introspective verbs to create a contemplative and introspective tone. Use minimal “be” verbs. Use a distant, third person omniscient voice. Reflect on the different scents, sounds, and sights of the scene. Use many wide-ranging descriptive adjectives and nouns to create a rich, vivid, and immersive scene. Please use 100 words to describe the description topic."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
-    )
-    return response['choices'][0]['message']['content']
+	"""Creates a text description of an transcription (ChatGPT-3.5 model)"""
+	response = client.chat.completions.create(
+		model="gpt-3.5-turbo",
+		temperature=0,
+		messages=[
+			{
+				"role": "system",
+				"content": "Describe this scene using the following instructions: Use contemplative and introspective verbs to create a contemplative and introspective tone. Use minimal “be” verbs. Use a distant, third person omniscient voice. Reflect on the different scents, sounds, and sights of the scene. Use many wide-ranging descriptive adjectives and nouns to create a rich, vivid, and immersive scene. Please use 70 words to describe the description topic."
+			},
+			{
+				"role": "user",
+				"content": transcription
+			}
+		]
+	)
+	print(response.choices[0].message.content)
+
+	return(response.choices[0].message.content)
 
 
-try:
-  response = client.images.create_variation(
-    image=open("image_edit_mask.png", "rb"),
-    n=1,
-    model="dall-e-2",
-    size="1024x1024"
-  )
-  print(response.data[0].url)
-except openai.OpenAIError as e:
-  print(e.http_status)
-  print(e.error)
+def image_from_prompt(description):
+	"""Creates an image from a given description (DALL-E-3 Model)"""
+	try:
+		response = client.images.generate(
+		model="dall-e-3",
+		prompt=description,
+		size="1024x1792",
+		quality="standard",
+		n=1,
+		)
+		image_url = response.data[0].url
+		return(image_url)
+	except openai.OpenAIError as e:
+		print(e.http_status)
+		print(e.error)
+
+def main():
+	audio_file_path = "/Users/anna/Downloads/QHACKS24/openai-env/harvard.wav"
+	transcription = transcribe_audio(audio_file_path)
+	description = image_prompt_from_text(transcription)
+	print(image_from_prompt(description))
+
+	
+main()
